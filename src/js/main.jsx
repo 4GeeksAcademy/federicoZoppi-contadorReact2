ef668b026ebb4dcb9e6b07f14c63e71b265fb09c
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-19 22:33:36 +0000

    federicoZoppi contadorSimple

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
-
-// index.css'
 import '../styles/index.css'
+import PropTypes from 'prop-types';
 
-// components
-import Home from './components/Home';
+function SimpleCounter(props) {
+  return (
+    <div className='bigCounter'>
+      <div className="calendar">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour}</div>
+      <div className="three">{props.digitThree}</div>
+      <div className="two">{props.digitTwo}</div>
+      <div className="one">{props.digitOne}</div>
+    </div>
+  )
+}
+SimpleCounter.propTypes = {
+  digitOne: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitFour: PropTypes.number
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+setInterval(function () {
+  const four = Math.floor(counter / 1000) % 10;
+  const three = Math.floor(counter / 100) % 10;
+  const two = Math.floor(counter / 10) % 10;
+  const one = Math.floor(counter / 1) % 10;
+  counter++;
+  root.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+  );
+}, 1000);
+
+
